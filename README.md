69052a298a0c5312931ed2c82597e356c522852c
Gyanendra <gyanen56@gmail.com>
2024-07-27 14:43:41 +1000

    Install morgan to track request

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # NOT TODO list API
 
+## Install express 
+>`npm i express`
+
+## Install morgan
+### to track the request made from where to the api end point
+>`npm i morgan`
--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import taskRouter from './src/routers/taskRouter.js'
+import morgan from 'morgan'
 const app = express()
 const PORT = 8000
 
+app.use(morgan("combined"))
 app.use(express.json())
 
 app.use('/api/v1/taskLists', taskRouter)

5a08f6ff47624eefe5abd39b3b050b9d181caba2
Gyanendra <gyanen56@gmail.com>
2024-08-24 11:28:13 +1000

    statci folder servering, adding .env file

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ import express from 'express';
 import taskRouter from './src/routers/taskRouter.js'
 import morgan from 'morgan'
 import cors from 'cors'
+import path from 'path';
 
 import { connectionMongoose } from './src/config/dbConfig.js';
+const __dirname = path.resolve()
+
 const app = express()
 const PORT = 8000
 
 app.use(morgan("combined"))
 app.use(cors())
+
+// static severing static folder
+app.use(express.static(path.join(__dirname,"dist")))
+
 app.use(express.json())
 connectionMongoose()
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"dist","index.html"))
+})
 app.use('/api/v1/taskLists', taskRouter)
 
 
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,9 +1,10 @@
 import { mongoose } from "mongoose";
-const str =
-  'mongodb+srv://Gyanendra:Gyan1234@nodeexpressproject.af7crgh.mongodb.net/online_ntdl?retryWrites=true&w=majority&appName=NodeExpressProject'
+import 'dotenv/config'
+
+
   export const connectionMongoose = ()=>{
 
-mongoose.connect(str).then(()=>console.log('Connected to DB')).catch((error)=>console.log('something went wrong'))
+mongoose.connect( process.env.MONGO_URI).then(()=>console.log('Connected to DB')).catch((error)=>console.log('something went wrong'))
         
    
   } 
